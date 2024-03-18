c5bdd356fa91deab0d5e2960d873e7e76bee9262
edsaibert <eduarda6saibert@gmail.com>
2024-03-18 09:54:01 -0300

    library include fix

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
+
 #include <iostream>
+#include <ogg/ogg.h>
 #include <vorbis/vorbisfile.h>
 #include <stdio.h>
 #include <vector>
-#include <fstream>
+// #include <fstream>
 
 using namespace std;
 
 int main(int agrc, char* argc[]) {
-    OggVorbis_File stream;
-    ofstream ogg;
-    ogg.open("audio.ogg", ios::binary);
+    OggVorbis_File *stream;      // Arquivo que inicia a bitstream de dados
+    FILE *ogg;                  // Arquivo onde será 'escrito' o áudio
+    char* buffer;               // Buffer FILE
 
-    if (ogg.is_open()){
-        // trocar a funcao para ov_open
-        ov_fopen( &stream);
+    ogg = fopen("audio.ogg", "r+");
+
+    if (ogg != NULL){
+        // ov_open(ogg, stream, NULL, 0);
+        printf("Open file");
+        // ov_clear(stream);
     }
+    else
+        return 0;
+
+    return 1;
 
 }
 
